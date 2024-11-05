345172be98ee351fd814e3d4e497b1ae6d9b52da
Andreina2 <andreinaarias5@gmail.com>
2024-11-05 21:47:21 +0100

    create methods and test of HandlerWeekDays

--- src/main/java/dev/andreinaarias/ex_list/service/HandlerWeekDays.java
+++ src/main/java/dev/andreinaarias/ex_list/service/HandlerWeekDays.java
@@ -1,6 +1,8 @@
 package dev.andreinaarias.ex_list.service;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class HandlerWeekDays {
@@ -15,21 +17,58 @@ public class HandlerWeekDays {
         createList();
     }
 
-    // Getter
+    // Getter y Setter: modifican y recuperan los atributos de la clase :
+    //Getter
     public List<String> getDays() {
         return days;
     }
 
-    // Setter
+    /*Setter
     public void setDays(List<String> days) {
     this.days = days;
+    }*/
+
+    public int getSizeList() {
+        return getDays().size();
     }
 
-    // Scope de un método, Scope de una clase
+    public boolean deleteDay(String day) {
+        return days.remove(day);
+    }
+
+    public String getSpecificDayByName (String Day) {
+
+        if (days.contains(Day)) {
+            return Day;
+        }
+        
+        
+        else {
+            return ("No se encuentra el día solicitado");
+        }
+    }
+
+    public boolean dayExist(String Day){
+        return days.contains(Day);
+        
+    }
+
+    // Collections.sort : ordena una lista de elementos en orden ascendente
+    public void sortByAlphabet() {
+        Collections.sort(days);
+
+    }
+
+    public void emptyList() {
+        days.clear();
+    }
+
+
+    // Scope de un método, Scope de una clase (lo Que esta dentro de la clase: dentro de las llaves)
     public void createList() {
 
-        //4 se ejecuta toda la logica
-        days =  new ArrayList();
+        // 4 se ejecuta toda la logica
+        days =  new ArrayList<String>();
         days.add("Lunes");
         days.add("Martes");
         days.add("Miercoles");
--- src/test/java/dev/andreinaarias/ex_list/service/HandlerWeekDaysTest.java
+++ src/test/java/dev/andreinaarias/ex_list/service/HandlerWeekDaysTest.java
@@ -2,6 +2,10 @@ package dev.andreinaarias.ex_list.service;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +16,66 @@ public class HandlerWeekDaysTest {
         HandlerWeekDays handlerWeekDays= new HandlerWeekDays(); //1 instancia
         //handlerWeekDays.createList();
         assertThat(handlerWeekDays.days.size(), is(7));
+        
+    }
+
+    @Test
+    void testGetDays() {
+        HandlerWeekDays handlerWeekDays= new HandlerWeekDays();
+        assertThat(handlerWeekDays.getDays().get(0), is("Lunes"));
+    }
+
+    @Test
+    void testGetSizeList() {
+        HandlerWeekDays handlerWeekDays= new HandlerWeekDays();
+        assertThat(handlerWeekDays.getSizeList(), is(7));
+        
+        List<String> days = handlerWeekDays.getDays();
+        assertThat(days, contains("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"));
+        
+        
+    }
+
+    @Test
+    void testDeleteDay() {
+        HandlerWeekDays handlerWeekDays= new HandlerWeekDays();
+        assertThat(handlerWeekDays.getDays(), contains("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"));
+        assertTrue(handlerWeekDays.deleteDay("Lunes"));
+        assertThat(handlerWeekDays.getDays().size(), is(6));
+        assertThat(handlerWeekDays.getDays(), not(contains("Lunes")));
+        System.out.println("Week Days: " + handlerWeekDays.getDays());
+        
+    }
+
+    @Test
+    void testGetSpecificDayByName() {
+        HandlerWeekDays handlerWeekDays= new HandlerWeekDays();
+        assertEquals("Martes",handlerWeekDays.getSpecificDayByName("Martes"));
+        
+    }
+
+    @Test
+    void testDayExist() {
+        HandlerWeekDays handlerWeekDays= new HandlerWeekDays();
+        assertFalse(handlerWeekDays.dayExist("Sunday"));
+        assertTrue(handlerWeekDays.dayExist("Jueves"));
+        
+    }
+
+    @Test
+    void testSortByAlphabet() {
+        HandlerWeekDays handlerWeekDays= new HandlerWeekDays();
+        handlerWeekDays.sortByAlphabet();
+        //System.out.println(handlerWeekDays.getDays());
+        
+    }
+
+    @Test
+    void testEmptyList() {
+        HandlerWeekDays handlerWeekDays= new HandlerWeekDays();
+        assertFalse(handlerWeekDays.getDays().isEmpty(), "La lista no debe estar vacía al principio");
+        handlerWeekDays.emptyList();
+
         
     }
 }
